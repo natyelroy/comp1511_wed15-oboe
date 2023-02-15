8fd0dd7a9b83d842dc50cad123c9f6abc94bf5d3
Natalie Leroy <z5363041@ad.unsw.edu.au>
2023-02-15 15:52:22 +1100

    Tute01 lab exercises

---
+++ tute01/cat.c
@@ -0,0 +1,14 @@
+// This will print Hello World and cat
+// Date: 15/02/2023
+// By Natalie Leroy
+
+#include <stdio.h>
+
+int main (void) {
+    // How do we print Hello world?
+    printf("Hello World\n");
+    printf("  /\\___/\\\n ");
+    printf("(=' w '=)\n");
+
+    return 0;
+}
